978ff29c061e85adde0a8699b11315a26a83c524
Eyal Schocken <schocken.eyal@gmail.com>
2017-12-27 13:02:14 +0200

    ex 09

---
+++ exercise_09/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.set('port', 3000);
+app.use("/files", express.static(__dirname + '/files'));
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+})
