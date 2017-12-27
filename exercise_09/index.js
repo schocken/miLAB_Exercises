25ec95cf357b7180ce32cc48d87d8d7d539b27e2
Eyal Schocken <schocken.eyal@gmail.com>
2017-12-27 12:54:50 +0200

    Revert "ex_09"
    This reverts commit e740978972e64640a349b165dbe2ca7fa556df88.

--- exercise_09/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.set('port', 3000);
-app.use("/files", express.static(__dirname + '/files'));
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
-});
